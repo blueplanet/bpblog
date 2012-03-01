da8e2cea1aaeeeea3203ee3ba5a7154f2507b98b
blueplanet <erguolinge@gmail.com>
2012-03-02 06:28:15 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bpblog::Application.config.secret_token = '8fc35d0460778dc622f0616396fc9d340307563c68189b9ee41a944017f3d5013aec956ec59e104fe525dc2c7b891b22120a7a24e4fc261d07541cf955466c49'
